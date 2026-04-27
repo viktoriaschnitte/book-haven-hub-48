b7e653061b8132695e083bf733f668b3846bedcb
Viktoria <_>
2026-04-27 16:25:14 +0200

    AppIcon

---
+++ public/sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'bookshelf-v1';
+const ASSETS = ['/', '/index.html', '/favicon.png'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => res || fetch(event.request))
+  );
+}); 
--- src/main.tsx
+++ src/main.tsx
@@ -3,3 +3,11 @@ import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg => console.log('Service Worker registriert!', reg))
+      .catch(err => console.error('Service Worker Fehler:', err));
+  });
+}
